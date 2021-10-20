c92ae75bf11a7eba994e84dc3460075487e33ada
Arthur Khakimov <verybigfolder@yandex.ru>
2021-10-21 01:46:14 +0500

    Переместил файлы сюда

---
+++ MatrixArithmetic/Matrix/MatrixExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MatrixArithmetic
+{
+    public class MatrixDifferentDimException : Exception
+    {
+        public MatrixDifferentDimException() : base("У этих матриц разная размерность")
+        {
+        }
+
+        public MatrixDifferentDimException(string message) : base(message)
+        {
+        }
+    }
+}

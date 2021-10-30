4591893cb0af86f760e31a194715f4bdf45bf130
Arthur Khakimov <verybigfolder@yandex.ru>
2021-10-31 01:08:31 +0500

    Impl TaxiCab norm

---
+++ MatrixArithmetic/Norms/INorma.cs
@@ -0,0 +1,8 @@
+﻿namespace MatrixArithmetic.Norms
+{
+    public interface INorma
+    {
+        public double VectorNorm(IVector<double> vector);
+        public double MatrixNorm(IMatrix<double> matrix);
+    }
+}
---
+++ MatrixArithmetic/Norms/TaxiCab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Math;
+
+namespace MatrixArithmetic.Norms
+{
+    public class TaxiCabNorm : INorma
+    {
+        public double VectorNorm(IVector<double> vector) => vector.Sum(Abs);
+
+        public double MatrixNorm(IMatrix<double> matrix)
+        {
+            IEnumerable<double> ColumnNorm(IMatrix<double> columns)
+            {
+                for (var i = 0; i < columns.N; i++)
+                {
+                    yield return VectorNorm(columns.GetColumn(i));
+                }
+            }
+
+            return ColumnNorm(matrix).Max();
+        }
+    }
+}

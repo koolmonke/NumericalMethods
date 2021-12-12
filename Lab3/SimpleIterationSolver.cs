6110082fb106ae550f1d0e181f5624ae85c3e038
Arthur Khakimov <verybigfolder@yandex.ru>
2021-12-12 19:02:01 +0500

    Пусть тут будет метод простых итераций

---
+++ Lab3/SimpleIterationSolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MatrixArithmetic;
+using MatrixArithmetic.Norms;
+
+namespace Lab3
+{
+    public class SimpleIterationSolver
+    {
+        public SimpleIterationSolver(Func<Vector, double>[] vectorFunction, Vector guess, INorma norm)
+        {
+            VectorFunction = vectorFunction;
+            Guess = guess;
+            Norm = norm;
+        }
+
+        public Func<Vector, double>[] VectorFunction { get; }
+        public Vector Guess { get; }
+        public INorma Norm { get; }
+
+        public int CounterIteration { get; private set; }
+
+        public Vector SolutionVector => _solutionVector ??= Solve();
+        
+        private Vector? _solutionVector;
+
+        private Vector Solve()
+        {
+            var xk = Guess;
+            Vector xkp;
+            do
+            {
+                xkp = xk;
+                xk = VectorFunction.Apply(xk);
+                CounterIteration++;
+            } while (Norm.VectorNorm(xk - xkp) > 1e-3);
+
+            return xk;
+        }
+    }
+}

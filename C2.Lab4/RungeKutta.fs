937441d56deccedcc53304c753f358470dd6e57d
Arthur Khakimov <verybigfolder@yandex.ru>
2022-04-11 17:30:26 +0500

    Более внятый оператор умножения

--- C2.Lab4/RungeKutta.fs
+++ C2.Lab4/RungeKutta.fs
@@ -1,6 +1,6 @@
 ﻿module C2.Lab4.RungeKutta
 
-let inline (@|>@) (left: float) (a1: float, a2: float) = (left * a1, left * a2)
+let inline ( *** ) (left: float) (a1: float, a2: float) = (left * a1, left * a2)
 
 let solve f12 n tau =
     let rec loop i (y1, y2) =
@@ -9,18 +9,18 @@ let solve f12 n tau =
 
             if i < (n - 1) then
                 let tN = tau * float i
-                let k11, k12 = tau @|>@ f12 tN y1 y2
+                let k11, k12 = tau *** f12 tN y1 y2
 
                 let k21, k22 =
                     tau
-                    @|>@ f12 (tN + tau / 2.) (y1 + k11 / 2.) (y2 + k12 / 2.)
+                    *** f12 (tN + tau / 2.) (y1 + k11 / 2.) (y2 + k12 / 2.)
 
                 let k31, k32 =
                     tau
-                    @|>@ f12 (tN + tau / 2.) (y1 + k21 / 2.) (y2 + k22 / 2.)
+                    *** f12 (tN + tau / 2.) (y1 + k21 / 2.) (y2 + k22 / 2.)
 
                 let k41, k42 =
-                    tau @|>@ f12 (tN + tau) (y1 + k31) (y2 + k32)
+                    tau *** f12 (tN + tau) (y1 + k31) (y2 + k32)
 
                 let newY1 =
                     y1 + (k11 + 2. * k21 + 2. * k31 + k41) / 6.

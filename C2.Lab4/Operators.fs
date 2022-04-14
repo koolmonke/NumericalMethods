e6ba805f70a47f17f4a9e3e6539b8f2fad851c0b
Arthur Khakimov <verybigfolder@yandex.ru>
2022-04-15 03:19:49 +0500

    Ура новые операторы

--- C2.Lab4/Operators.fs
+++ C2.Lab4/Operators.fs
@@ -1,3 +1,5 @@
 ﻿module C2.Lab4.Operators
 
-let ( *** ) (left: float) (a1: float, a2: float) = (left * a1, left * a2)
+let ( .* ) (left: float) (a1: float, a2: float) = (left * a1, left * a2)
+let ( ++ ) (a1: float, a2: float) (b1: float, b2: float) = (a1 + b1, a2 + b2)
+let ( -- ) (a1: float, a2: float) (b1: float, b2: float) = (a1 - b1, a2 - b2)

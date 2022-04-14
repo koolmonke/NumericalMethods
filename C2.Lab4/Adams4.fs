201648f473109f8c34acf8fe62bac1e317bc1caf
Arthur Khakimov <verybigfolder@yandex.ru>
2022-04-15 03:25:02 +0500

    Написал метод адамса

---
+++ C2.Lab4/Adams4.fs
@@ -0,0 +1,41 @@
+﻿module C2.Lab4.Adams4
+
+open System
+open MatrixArithmetic
+open Operators
+
+let solve f12 (n: int) tau rungeInits =
+    let rungeKutta =
+        RungeKutta.solve f12 n tau rungeInits
+        |> Seq.take 5
+        |> Seq.toArray
+
+    let y1 = Vector(n)
+    let y2 = Vector(n)
+    for i in 0 .. 4 do
+        y1.[i] <- fst rungeKutta.[i]
+        y2.[i] <- snd rungeKutta.[i]
+
+    let adams =
+        seq {
+            for i in 4 .. (n - 2) do
+                let tN = tau * float i
+
+                let newY1, newY2 =
+                    (y1.[i], y2.[i])
+                    ++ (tau
+                         .* (
+                              ((55. / 24.) .* f12 tN y1.[Index(i - 1)] y2.[Index(i - 1)])
+                              -- ((59. / 24.) .* f12 tN y1.[Index(i - 2)] y2.[Index(i - 2)])
+                              ++ ((37. / 24.) .* f12 tN y1.[Index(i - 3)] y2.[Index(i - 3)])
+                              -- ((3. / 8.) .* f12 tN y1.[Index(i - 4)] y2.[Index(i - 4)])
+                              )
+                         )
+
+                y1.[Index(i + 1)] <- newY1
+                y2.[Index(i + 1)] <- newY2
+                yield newY1, newY2
+        }
+
+    Seq.concat [ Array.toSeq rungeKutta 
+                 adams ]
--- C2.Lab4/Program.fs
+++ C2.Lab4/Program.fs
@@ -11,7 +11,7 @@ let h = 0.1
 
 let alpha = 2. + 0.5 * float var
 
-let n = 1 + int (T / h)
+let n = 2 + int (T / h)
 
 let f1 t y1 y2 = sin (alpha * y1 ** 2.) + t + y2
 
@@ -23,5 +23,5 @@ let f12 t y1 y2 = f1 t y1 y2, f2 t y1 y2
 printfn "t     y1       y2       y3       y4"
 
 for t, (y1, y2), (y3, y4) in
-    Seq.zip3 (seq { 0.0 .. 0.1 .. 1.0 }) (Hoyna.solve f1 f2 n h (1., 0.5)) (Euler.solve f1 f2 n h (1., 0.5)) do
+    Seq.zip3 (seq { 0.0 .. 0.1 .. 1.0 }) (Hoyna.solve f1 f2 n h (1., 0.5)) (Adams4.solve f12 n h (1., 0.5)) do
     printfn $"%.1f{t} %8.4f{y1} %8.4f{y2} %8.4f{y3} %8.4f{y4}"

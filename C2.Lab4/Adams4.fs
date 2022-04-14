761a74ecb6536d15e562e72fb3fac65f1cc402d3
Arthur Khakimov <verybigfolder@yandex.ru>
2022-04-15 04:22:26 +0500

    Нам достаточно иметь кэш в 5 элементов

--- C2.Lab4/Adams4.fs
+++ C2.Lab4/Adams4.fs
@@ -4,14 +4,14 @@ open System
 open MatrixArithmetic
 open Operators
 
-let solve f12 (n: int) tau rungeInits =
+let solve f12 n tau rungeInits =
     let rungeKutta =
         RungeKutta.solve f12 n tau rungeInits
         |> Seq.take 5
         |> Seq.toArray
 
-    let y1 = Vector(n)
-    let y2 = Vector(n)
+    let y1 = Vector(5)
+    let y2 = Vector(5)
     for i in 0 .. 4 do
         y1.[i] <- fst rungeKutta.[i]
         y2.[i] <- snd rungeKutta.[i]
@@ -22,18 +22,18 @@ let solve f12 (n: int) tau rungeInits =
                 let tN = tau * float i
 
                 let newY1, newY2 =
-                    (y1.[i], y2.[i])
+                    (y1.[Index(i % 5)], y2.[Index(i % 5)])
                     ++ (tau
                          .* (
-                              ((55. / 24.) .* f12 tN y1.[Index(i - 1)] y2.[Index(i - 1)])
-                              -- ((59. / 24.) .* f12 tN y1.[Index(i - 2)] y2.[Index(i - 2)])
-                              ++ ((37. / 24.) .* f12 tN y1.[Index(i - 3)] y2.[Index(i - 3)])
-                              -- ((3. / 8.) .* f12 tN y1.[Index(i - 4)] y2.[Index(i - 4)])
+                              ((55. / 24.) .* f12 tN y1.[Index((i - 1) % 5)] y2.[Index((i - 1) % 5)])
+                              -- ((59. / 24.) .* f12 tN y1.[Index((i - 2) % 5)] y2.[Index((i - 2) % 5)])
+                              ++ ((37. / 24.) .* f12 tN y1.[Index((i - 3) % 5)] y2.[Index((i - 3) % 5)])
+                              -- ((3. / 8.) .* f12 tN y1.[Index((i - 4) % 5)] y2.[Index((i - 4) % 5)])
                               )
                          )
 
-                y1.[Index(i + 1)] <- newY1
-                y2.[Index(i + 1)] <- newY2
+                y1.[Index((i + 1) % 5)] <- newY1
+                y2.[Index((i + 1) % 5)] <- newY2
                 yield newY1, newY2
         }
 

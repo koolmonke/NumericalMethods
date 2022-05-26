1d8e3b244ef1572bd6fd35c8b079ce62298ef3fa
Arthur Khakimov <verybigfolder@yandex.ru>
2022-05-26 17:35:48 +0500

    Уберем точки из индекса

--- C2.Lab4/Adams4.fs
+++ C2.Lab4/Adams4.fs
@@ -13,8 +13,8 @@ let solve f12 n tau rungeInits =
     let y1 = Vector(5)
     let y2 = Vector(5)
     for i in 0 .. 4 do
-        y1.[i] <- fst rungeKutta.[i]
-        y2.[i] <- snd rungeKutta.[i]
+        y1[i] <- fst rungeKutta[i]
+        y2[i] <- snd rungeKutta[i]
 
     let adams =
         seq {
@@ -22,18 +22,18 @@ let solve f12 n tau rungeInits =
                 let tN = tau * float i
 
                 let newY1, newY2 =
-                    (y1.[Index(i % 5)], y2.[Index(i % 5)])
+                    (y1[Index(i % 5)], y2[Index(i % 5)])
                     ++ (tau
                          .* (
-                              ((55. / 24.) .* f12 tN y1.[Index((i - 1) % 5)] y2.[Index((i - 1) % 5)])
-                              -- ((59. / 24.) .* f12 tN y1.[Index((i - 2) % 5)] y2.[Index((i - 2) % 5)])
-                              ++ ((37. / 24.) .* f12 tN y1.[Index((i - 3) % 5)] y2.[Index((i - 3) % 5)])
-                              -- ((3. / 8.) .* f12 tN y1.[Index((i - 4) % 5)] y2.[Index((i - 4) % 5)])
+                              ((55. / 24.) .* f12 tN y1[Index((i - 1) % 5)] y2[Index((i - 1) % 5)])
+                              -- ((59. / 24.) .* f12 tN y1[Index((i - 2) % 5)] y2[Index((i - 2) % 5)])
+                              ++ ((37. / 24.) .* f12 tN y1[Index((i - 3) % 5)] y2[Index((i - 3) % 5)])
+                              -- ((3. / 8.) .* f12 tN y1[Index((i - 4) % 5)] y2[Index((i - 4) % 5)])
                               )
                          )
 
-                y1.[Index((i + 1) % 5)] <- newY1
-                y2.[Index((i + 1) % 5)] <- newY2
+                y1[Index((i + 1) % 5)] <- newY1
+                y2[Index((i + 1) % 5)] <- newY2
                 yield newY1, newY2
         }
 
